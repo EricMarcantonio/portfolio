bf82ab72b517812411307620c1ebeb2d3cd8a21b
Eric Marcantonio <eric11marcantonio@gmail.com>
2023-02-25 11:03:44 -0500

    adding images

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: "Mr. Marcantonio's Portfolio",
+    description: 'Checkout my experience as a teacher',
+    image: '/images/bitmoji.png'
   }
 
   return (
